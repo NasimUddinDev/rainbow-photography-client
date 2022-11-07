1562b9c0626d9606502078a8b69fd8a643d42891
nasim0994 <nasimuddin0994@gmail.com>
2022-11-08 00:14:54 +0600

    signup page added

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -2,16 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 
-const Login = () => {
+const Signup = () => {
   return (
     <div className="hero bg-base-200 py-4">
       <div className="hero-content flex-col w-1/3">
         <div className="text-center">
-          <h1 className="text-5xl font-bold">Log In</h1>
+          <h1 className="text-5xl font-bold">Sign Up</h1>
         </div>
 
         <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
           <form className="card-body">
+            <div className="form-control">
+              <input
+                type="text"
+                placeholder="Name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <input
+                type="text"
+                placeholder="Img Link"
+                className="input input-bordered"
+                required
+              />
+            </div>
             <div className="form-control">
               <input
                 type="email"
@@ -27,23 +43,19 @@ const Login = () => {
                 className="input input-bordered"
                 required
               />
-              <label className="label">
-                <Link to="" className="label-text-alt link link-hover">
-                  Forgot password?
-                </Link>
-              </label>
 
+              {/* Error Message */}
               <div>
                 <h3 className="text-red-500">{}</h3>
               </div>
             </div>
 
             <div className="form-control mt-3">
-              <button className="btn btn-active">Login</button>
+              <button className="btn btn-active">SignUp</button>
             </div>
 
             <div>
-              <p className="text-center">Or login with</p>
+              <p className="text-center">Or signup with</p>
               <button className="btn btn-active w-full mt-3">
                 <FcGoogle className="mr-2" /> <span>Google</span>
               </button>
@@ -51,9 +63,9 @@ const Login = () => {
 
             <div className="text-center">
               <small>
-                Not an account?{" "}
-                <Link to="/signup" className="underline ">
-                  Sign Up
+                already have an account?{" "}
+                <Link to="/login" className="underline ">
+                  Log In
                 </Link>
               </small>
             </div>
@@ -64,4 +76,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Signup;
--- src/Router/Routes.js
+++ src/Router/Routes.js
@@ -3,6 +3,7 @@ import Home from "../Pages/Home/Home";
 import Layout from "./../Layout/Layout";
 import Blog from "./../Pages/Blog/Blog";
 import Login from "./../Pages/Login/Login";
+import Signup from "./../Pages/Signup/Signup";
 
 const router = createBrowserRouter([
   {
@@ -24,6 +25,10 @@ const router = createBrowserRouter([
       {
         path: "/login",
         element: <Login></Login>,
+      },
+      {
+        path: "/signup",
+        element: <Signup></Signup>,
       },
     ],
   },
